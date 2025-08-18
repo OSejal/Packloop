ab546ed0c1acd3db9fe3ce34f7ecf844246387d2
Sejal <106868816+Sejal1411@users.noreply.github.com>
2025-08-18 17:13:20 +0530

    Added profile routes

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const profileController = require('../controllers/profileController');
 const { verifyToken, authorize } = require('../middlewares/auth');
 
-router.get('/get', authorize, getProfile);        
-router.post('/edit', authorize, saveProfile);     
+router.use(verifyToken);
+
+router.get('/get', authorize, profileController.getProfile);        
+router.post('/edit', authorize, profileController.saveProfile);     
 
 module.exports = router;
