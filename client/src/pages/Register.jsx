e11b7d9e0f7233982ebceffd49083dc6b85477ce
Sejal <106868816+Sejal1411@users.noreply.github.com>
2025-08-19 15:49:24 +0530

    Remodified Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -91,7 +91,7 @@ const Register = () => {
   try {
     // Use the environment variable for the backend URL
     const API_URL = import.meta.env.VITE_API_URL;
-    const res = await fetch(`${API_URL}/api/auth`, {
+    const res = await fetch(`${API_URL}/api/auth/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(userData),
