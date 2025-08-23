e94fdf739049aff2b82b4b146b6d348d138f511c
Sejal <106868816+Sejal1411@users.noreply.github.com>
2025-08-23 12:39:13 +0530

    Modified pickup

--- server/routes/pickup.js
+++ server/routes/pickup.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const router = express.Router();
-const { createOrder } = require("../controllers/paymentController");
+// const express = require("express");
+// const router = express.Router();
+// const { createOrder } = require("../controllers/paymentController");
 
-router.post("/create-order", createOrder);
+// router.post("/create-order", createOrder);
 
-module.exports = router;
+// module.exports = router;
